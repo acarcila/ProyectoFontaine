e3e701380946a3d57b64435fe3f129a90f3fc179
acarcila <acarcila98@gmail.com>
2018-09-24 21:45:07 -0500

    Se cambió la barra de progreso de tiempo por una barra de progreso de preguntas

---
+++ ProyectoFontaine/Assets/Elementos/No interactuables/BarrasProgreso/BarraProgresoPreguntas/BarraProgresoTiempo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraProgresoTiempo : MonoBehaviour {
+
+	public int numeroPreguntasMaximo;
+    public int numeroPreguntas;
+	public bool over;
+
+    private Image imageProgreso;
+
+	// Use this for initialization
+	void Start () {
+		imageProgreso = GameObject.Find(this.gameObject.name + "/ImageProgreso").GetComponent<Image>();
+		over = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (numeroPreguntas >= numeroPreguntasMaximo)
+        {
+            over = true;
+        }
+        else
+        {
+            over = false;
+        }
+
+		imageProgreso.fillAmount = ((float)numeroPreguntas / numeroPreguntasMaximo);
+	}
+
+    public void aumentarPregunta()
+    {
+        if (numeroPreguntas < numeroPreguntasMaximo)
+        {
+            numeroPreguntas++;
+        }
+    }
+}

1fc8292f626c3873fcd4d8aa167d21c2d69e132a
acarcila <acarcila98@gmail.com>
2018-09-25 00:46:45 -0500

    Se creó la selección de preguntas aleatorias y se crearon las clases Pregunta y Opcion

---
+++ ProyectoFontaine/Assets/Elementos/GameManager/Pregunta.cs
@@ -0,0 +1,9 @@
+﻿[System.Serializable]
+public class Pregunta{
+	public string pregunta;
+	public new Opcion opcion1;
+	public new Opcion opcion2;
+	public new Opcion opcion3;
+	public new Opcion opcion4;
+	public int opcionCorrecta;
+}

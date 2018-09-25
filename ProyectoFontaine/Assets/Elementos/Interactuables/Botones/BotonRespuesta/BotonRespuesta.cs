6fe9123367131c2ce0b9566f2267e461f894eb0d
acarcila <acarcila98@gmail.com>
2018-09-24 23:40:36 -0500

    Se creó el prefab de los botones de las respuestas

---
+++ ProyectoFontaine/Assets/Elementos/Interactuables/Botones/BotonRespuesta/BotonRespuesta.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonRespuesta : MonoBehaviour {
+
+    public string textoRespuesta;
+    public bool verdadera;
+    public int numero;
+
+    private Text textoBoton;
+
+	// Use this for initialization
+	void Start () {
+        actualizarValores();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void actualizarValores()
+    {
+        textoBoton = GetComponentInChildren<Text>();
+        textoBoton.text = textoRespuesta;
+    }
+}

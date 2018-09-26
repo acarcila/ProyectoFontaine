018a95dfd2edaa822bba385aaedcbaf239a5ddd6
acarcila <acarcila98@gmail.com>
2018-09-26 00:01:58 -0500

    Se creó el panel para el valor de la respuesta, encargado de indicar si la respuesta del usuario fue acertada o no

---
+++ ProyectoFontaine/Assets/Elementos/No interactuables/PanelRespuesta/PanelRespuesta.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelRespuesta : MonoBehaviour {
+
+	public bool valorRespuesta;
+	public Color colorRespuestaCorrecta;
+	public Color colorRespuestaIncorrecta;
+	public string textoRespuestaCorrecta;
+	public string textoRespuestaIncorrecta;
+	private Text textoValorRespuesta;
+	
+	void Start () {
+		textoValorRespuesta = GetComponentInChildren<Text>();
+	}
+	// Update is called once per frame
+	void Update () {
+		actualizarElementosUI();
+	}
+
+	public void actualizarElementosUI()
+	{
+		if(valorRespuesta)
+		{
+			GetComponent<Image>().color = colorRespuestaCorrecta;
+			textoValorRespuesta.text = textoRespuestaCorrecta;
+		}
+		else
+		{
+			GetComponent<Image>().color = colorRespuestaIncorrecta;
+			textoValorRespuesta.text = textoRespuestaIncorrecta;
+		}
+	}
+}

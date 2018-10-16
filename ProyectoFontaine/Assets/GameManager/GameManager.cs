fe1fc222db43474b063f985d695e3b73505ea640
acarcila <acarcila98@gmail.com>
2018-10-16 11:33:14 -0500

    Se creó un manager para todo el juego en donde por el momento se está gestionando la conexión con la Base de Datos

---
+++ ProyectoFontaine/Assets/GameManager/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using MySql.Data.Common;
+using MySql.Data.Types;
+using MySql.Data.MySqlClient;
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public string servidorBaseDatos;
+	public string nombreBaseDatos;
+	public string usuarioBaseDatos;
+	public string contraseniaBaseDatos;
+	
+	private string datosConexion;
+	private MySqlConnection conexion;
+	// Use this for initialization
+	void Start () {
+		datosConexion = "Server=" + servidorBaseDatos + ";Database=" + nombreBaseDatos + ";Uid=" + usuarioBaseDatos + ";Pwd=" + contraseniaBaseDatos + ";";
+
+		conectarServidorBD();
+
+		MySqlDataReader resultado = select("`preguntas`");
+		DataTable tabla = new DataTable();
+		tabla.Load(resultado);
+		
+		foreach(DataRow row in tabla.Rows)
+		{
+			Debug.Log(row["pregunta"].ToString());
+		}
+
+		resultado.Close();
+
+		
+	}
+	
+	private void conectarServidorBD()
+	{
+		conexion = new MySqlConnection(datosConexion);
+
+		try
+		{
+			conexion.Open();
+			Debug.Log("Conexión exitosa con la base de datos");
+		}catch(MySqlException ex)
+		{
+			Debug.LogError("Imposible conextarse a la Base de Datos: " + ex);
+		}
+	}
+
+	public MySqlDataReader select(string selectInfo)
+	{
+		MySqlCommand cmd = conexion.CreateCommand();
+		cmd.CommandText = "SELECT * FROM " + selectInfo;
+		MySqlDataReader resultado = cmd.ExecuteReader();
+
+		return resultado;
+	}
+}

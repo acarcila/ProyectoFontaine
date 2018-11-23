9a5d3444d13d07813276a716b592850b06316b15
acarcila <acarcila98@gmail.com>
2018-11-23 13:43:45 -0500

    Se creó una barra de progreso circular y un botón

---
+++ ProyectoFontaine/Assets/Elementos/No interactuables/BarrasProgreso/BarraProgresoCircular/BarraProgresoCircularBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraProgresoCircularBehaviour : MonoBehaviour {
+
+	public Image imageBarraProgresoCircular;
+	public Text textValor;
+	public float valor;
+	public float valorMaximo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		imageBarraProgresoCircular.fillAmount = valor / valorMaximo;
+		textValor.text = valor + "";
+	}
+}

edb6bdc20252665b246a89a09cf428642e79727a
acarcila <acarcila98@gmail.com>
2018-11-23 13:46:04 -0500

    Se creó la pantalla de calificaciones

---
+++ ProyectoFontaine/Assets/GameManager/Calificacion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calificacion
+{
+    public int idCalificacion;
+    public int idPregunta;
+    public string idEstudiante;
+	public int nota;
+	public int tiempo;
+
+    public Calificacion(int idCalificacion, int idPregunta, string idEstudiante, int nota, int tiempo)
+    {
+		this.idCalificacion = idCalificacion;
+		this.idPregunta = idPregunta;
+        this.idEstudiante = idEstudiante;
+		this.nota = nota;
+		this.tiempo = tiempo;
+    }
+}
---
+++ ProyectoFontaine/Assets/GameManager/CalificationsManager/CalificationsManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalificationsManager : MonoBehaviour
+{
+
+	public BarraProgresoCircularBehaviour barraProgresoCalificacion;
+	public BarraProgresoCircularBehaviour barraProgresoNumeroIntentos;
+	public BarraProgresoCircularBehaviour barraProgresoRespuestasCorrectas;
+	public BarraProgresoCircularBehaviour barraProgresoRespuestasIncorrectas;
+
+    private GameManager gameManager;
+	private Calificacion[] calificaciones;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        gameManager = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
+
+		calificaciones = gameManager.getRespuestas();
+
+		List<Calificacion> mejoresCalificaciones = encontrarMejoresCalificaciones(calificaciones);
+		Calificacion mejorcalificacion = encontrarMejoresTiemposDeCalificaciones(mejoresCalificaciones);
+		barraProgresoCalificacion.valor = mejorcalificacion.nota;
+		barraProgresoCalificacion.valorMaximo = 5f;
+
+		barraProgresoNumeroIntentos.valor = calificaciones.Length;
+		barraProgresoNumeroIntentos.valorMaximo = calificaciones.Length;
+    }
+
+    private List<Calificacion> encontrarMejoresCalificaciones(Calificacion[] calificaciones)
+    {
+		float mejorNota = 0f;
+		List<Calificacion> mejoresCalificaciones = new List<Calificacion>();
+        for(int i = 0; i < calificaciones.Length; i++)
+		{
+			if(calificaciones[i].nota > mejorNota)
+			{
+				mejorNota = calificaciones[i].nota;
+			}
+		}
+
+		for(int i = 0; i < calificaciones.Length; i++)
+		{
+			if(calificaciones[i].nota == mejorNota)
+			{
+				mejoresCalificaciones.Add(calificaciones[i]);
+			}
+		}
+
+		return mejoresCalificaciones;
+    }
+
+	private Calificacion encontrarMejoresTiemposDeCalificaciones(List<Calificacion> mejoresCalificaciones)
+    {
+        int mejorTiempo = mejoresCalificaciones[0].tiempo;
+		Calificacion mejorTiempoCalificacion = null;
+        for(int i = 0; i < calificaciones.Length; i++)
+		{
+			if(calificaciones[i].nota < mejorTiempo)
+			{
+				mejorTiempoCalificacion = calificaciones[i];
+			}
+		}
+
+		return mejorTiempoCalificacion;
+    }
+
+}
